85da1d5075409018b64127b87c12efbba57533c7
Zheal18 <gallendos@gmail.com>
2026-05-29 17:53:39 +0800

    fix missing route closure

--- backend/server.js
+++ backend/server.js
@@ -436,6 +436,7 @@ app.post("/api/report/reply/:ticket", (req, res) => {
 );
 
 });
+});
 app.get("/api/report/messages/:ticket", (req, res) => {
   db.query(
   "SELECT id FROM reports WHERE ticket=?",
